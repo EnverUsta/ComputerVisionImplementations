7d9ddac10b5892087542d33a04aae73db753a2dc
Enver <enver.usta.3@gmail.com>
2020-07-31 10:44:20 +0300

    update

--- WEEK4/feature-matching/feature-matching.py
+++ WEEK4/feature-matching/feature-matching.py
@@ -1,34 +1,34 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread('images/book.png', cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread('images/holding_book.jpeg', cv2.IMREAD_GRAYSCALE)
 
-#ORB detector
-orb = cv2.ORB_create()
-kp1, des1 = orb.detectAndCompute(img1, None)
-kp2, des2 = orb.detectAndCompute(img2, None)
-
-#Brute force matching
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-#Match descriptors
-matches = bf.match(des1, des2)
-
-#Sort them in the order of their distance
-matches = sorted(matches, key=lambda x:x.distance)
-
-#Draw first 10 matches
-matching_result = cv2.drawMatches(img1, kp1, img2, kp2, matches, None)
+def feature_match(img1, img2):
+    orb = cv2.ORB_create()
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x:x.distance)
+    matching_result = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=2)
+    return matching_result
 
 
 
-cv2.imshow('img1', img1)
-cv2.imshow('img2', img2)
-cv2.imshow('result', matching_result)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10, 150)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+while True:
+    success, capImage = cap.read()
+    imgBook = cv2.imread('images/book.png')
+    matching_result = feature_match(imgBook, capImage)
+    cv2.imshow('Matching Result', matching_result)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
 
 
 
